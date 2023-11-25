e328c82ea43f702a4cf5b2f7f0798325ad0badd3
abhixyz1 <irfanhendra90@gmail.com>
2023-11-26 06:23:34 +0700

    tugas persegi angka

--- PersegiAngka.java
+++ PersegiAngka.java
@@ -11,10 +11,13 @@ public class PersegiAngka {
         for (int i=1;i <= n; i++ ) {
             for (int j=1; j <= n; j++) {
                 if (i==1||i==n||j==1||j==n) {
-                    System.out.print(n);
+                    System.out.print(""+n+" ");
+                }
+                else if (n>9) {
+                    System.out.print("   ");
                 }
                 else{
-                    System.out.print(" ");
+                    System.out.print("  ");
                 }
             } 
         System.out.println();
